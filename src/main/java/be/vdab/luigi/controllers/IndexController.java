1490861f708b4434a32b9508528cc68ad9cd5a4b
DoganHaluk <ha_look@yahoo.com>
2021-04-21 16:05:09 +0200

    Pizza luigi webpage

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -3,7 +3,6 @@ package be.vdab.luigi.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalTime;
