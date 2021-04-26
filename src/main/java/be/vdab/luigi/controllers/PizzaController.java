ced9b9822b114346ccf7c9c5cb3f96bc146b67e9
DoganHaluk <ha_look@yahoo.com>
2021-04-26 22:58:57 +0200

    Pizza luigi webpage

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -3,10 +3,12 @@ package be.vdab.luigi.controllers;
 import be.vdab.luigi.domain.Pizza;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -23,6 +25,14 @@ class PizzaController {
         var modelAndView = new ModelAndView("pizzas", "pizzas", pizzas);
         modelAndView.addObject("getallen", List.of(3, 7));
         modelAndView.addObject("landen", Map.of("B", "België", "NL", "Nederland"));
+        return modelAndView;
+    }
+
+    @GetMapping("{id}")
+    public ModelAndView pizza(@PathVariable long id) {
+        var modelAndView = new ModelAndView("pizza");
+        Arrays.stream(pizzas).filter(pizza -> pizza.getId() == id).findFirst()
+                .ifPresent(pizza -> modelAndView.addObject("pizza", pizza));
         return modelAndView;
     }
 }
