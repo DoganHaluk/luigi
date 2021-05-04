799948f125d7003d554c2e1fdd036860f3c288e5
DoganHaluk <ha_look@yahoo.com>
2021-05-04 07:48:06 +0200

    Pizza luigi webpage

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("pizzas")
@@ -33,6 +34,30 @@ class PizzaController {
         var modelAndView = new ModelAndView("pizza");
         Arrays.stream(pizzas).filter(pizza -> pizza.getId() == id).findFirst()
                 .ifPresent(pizza -> modelAndView.addObject("pizza", pizza));
+        return modelAndView;
+    }
+
+    private List<BigDecimal> uniekePrijzen() {
+        return Arrays.stream(pizzas)
+                .map(Pizza::getPrijs)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+    @GetMapping("prijzen")
+    public ModelAndView prijzen() {
+        return new ModelAndView("prijzen", "prijzen", uniekePrijzen());
+    }
+
+    private List<Pizza> pizzasMetPrijs(BigDecimal prijs) {
+        return Arrays.stream(pizzas)
+                .filter(pizza -> pizza.getPrijs().compareTo(prijs) == 0)
+                .collect(Collectors.toList());
+    }
+    @GetMapping("prijzen/{prijs}")
+    public ModelAndView pizzasMetEenPrijs(@PathVariable BigDecimal prijs) {
+        var modelAndView = new ModelAndView("prijzen", "pizzas", pizzasMetPrijs(prijs));
+        modelAndView.addObject("prijzen", uniekePrijzen());
         return modelAndView;
     }
 }
