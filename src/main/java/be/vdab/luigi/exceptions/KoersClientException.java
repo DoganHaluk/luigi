3102e997d4cfd9ad0546b874e88c1eb86470241b
DoganHaluk <ha_look@yahoo.com>
2021-05-10 02:58:59 +0200

    Pizza luigi webpage

--- src/main/java/be/vdab/luigi/exceptions/KoersClientException.java
+++ src/main/java/be/vdab/luigi/exceptions/KoersClientException.java
@@ -1,10 +1,12 @@
 package be.vdab.luigi.exceptions;
 
-public class KoersClientException extends RuntimeException{
+public class KoersClientException extends RuntimeException {
     private static final long serialVersionUID = 1L;
+
     public KoersClientException(String message) {
         super(message);
     }
+
     public KoersClientException(String message, Exception origineleFout) {
         super(message, origineleFout);
     }
--- src/main/java/be/vdab/luigi/restclients/FixerKoersClient.java
+++ src/main/java/be/vdab/luigi/restclients/FixerKoersClient.java
@@ -10,24 +10,30 @@ import java.net.URL;
 import java.util.regex.Pattern;
 
 @Component
-class FixerKoersClient implements KoersClient{
+class FixerKoersClient implements KoersClient {
     private static final Pattern PATTERN = Pattern.compile("^.*\"USD\": *(\\d+\\.?\\d*).*$");
     private final URL url;
+
     FixerKoersClient() {
         try {
-            url = new URL( "http://data.fixer.io/api/latest?access_key=TypHierJeFreeKey&symbols=USD");
+            url = new URL("http://data.fixer.io/api/latest?access_key=123a6790e81d1564f6867c97893f3c7e&symbols=USD");
         } catch (MalformedURLException ex) {
             throw new KoersClientException("Fixer URL is verkeerd.");
         }
     }
-    @Override public BigDecimal getDollarKoers() {
+
+    @Override
+    public BigDecimal getDollarKoers() {
+        var woord = "";
         try (var stream = url.openStream()) {
             var matcher = PATTERN.matcher(new String(stream.readAllBytes()));
             if (!matcher.matches()) {
-                throw new KoersClientException("Fixer data ongeldig", ex);
+                throw new KoersClientException("Fixer data ongeldig");
             }
             return new BigDecimal(matcher.group(1));
         } catch (IOException ex) {
+            woord = "faut";
+            System.out.println(woord);
             throw new KoersClientException("Kan koers niet lezen via Fixer.", ex);
         }
     }
