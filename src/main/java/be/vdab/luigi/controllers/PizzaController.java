d854a66012ac70b126b85d730b662b18cd03f562
DoganHaluk <ha_look@yahoo.com>
2021-06-09 22:49:39 +0200

    Pizza luigi webpage

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.math.BigDecimal;
@@ -81,11 +82,11 @@ class PizzaController {
     }
 
     @PostMapping
-    public ModelAndView toevoegen(@Valid Pizza pizza, Errors errors) {
+    public String toevoegen(@Valid Pizza pizza, Errors errors, RedirectAttributes redirect) {
         if (errors.hasErrors()) {
-            return new ModelAndView("toevoegen");
+            return "toevoegen";
         }
-        pizzaService.create(pizza);
-        return new ModelAndView("pizzas", "pizzas", pizzaService.findAll());
+        redirect.addAttribute("idNieuwePizza", pizzaService.create(pizza));
+        return "redirect:/pizzas";
     }
 }
